2679e50b2d3699599dafc0d588bf6fb38e3a4d2d
anujofficial9719-eng <anujofficial9719@gmail.com>
2026-03-19 03:12:29 +0530

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -5,16 +5,16 @@ aria2 = aria2p.API(
     aria2p.Client(host="http://localhost", port=6800)
 )
 
-def download_file(url, path="downloads"):
-    os.makedirs(path, exist_ok=True)
+def download(url):
+    os.makedirs("downloads", exist_ok=True)
 
-    download = aria2.add_uris([url], options={
-        "dir": path,
+    d = aria2.add_uris([url], options={
+        "dir": "downloads",
         "split": "16",
         "max-connection-per-server": "16"
     })
 
-    while not download.is_complete:
-        download.update()
+    while not d.is_complete:
+        d.update()
 
-    return download.files[0].path
+    return d.files[0].path
